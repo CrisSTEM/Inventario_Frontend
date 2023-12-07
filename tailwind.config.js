15644dcb925dd2463ca9b2567ee0a5a1e67c06f8
An Garcia <cristianagguniversity@gmail.com>
2023-12-07 13:20:08 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Asegúrate de incluir todos los archivos donde usarás Tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
